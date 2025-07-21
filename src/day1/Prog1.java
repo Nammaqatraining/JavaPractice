76dc62c60e0129df85343e800ba2a90027331b83
VinayNCLK <vinayn348@gmail.com>
2025-07-21 08:44:29 +0530

    first commit

---
+++ src/day1/Prog1.java
@@ -0,0 +1,21 @@
+package day1;
+
+
+
+import java.lang.*;
+
+
+public class Prog1 {
+	
+	public static void main(String[] args) throws java.io.IOException {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to nammaqa bng from day1");
+		
+		java.io.BufferedReader br = new java.io.BufferedReader(new java.io.InputStreamReader(System.in));
+		System.out.println("Please enter your name");
+		System.out.println("Your name is "+br.readLine());
+	
+		
+	}
+
+}
